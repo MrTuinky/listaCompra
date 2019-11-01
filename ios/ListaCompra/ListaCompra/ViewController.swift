c3e832fe6484fe1369859e3a386e7637ba912b0a
Carlos <carlos95egibide@gmail.com>
2019-11-01 17:17:24 +0100

    Proyecto nuevo creado

---
+++ ios/ListaCompra/ListaCompra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListaCompra
+//
+//  Created by Carlos Hernández on 01/11/2019.
+//  Copyright © 2019 Carlos Hernandez cascajares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
